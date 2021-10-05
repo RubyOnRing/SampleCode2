23ca60e4fc62d8c28f2e0d266b3cbcba0a24cf79
Duyet Nguyen React+ <88522317+duyetrubyonring@users.noreply.github.com>
2021-10-05 11:43:38 +0700

    apply core ui (#2)

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+  def sidebar_active(sidebar_path)
+    'c-active' if current_page?(sidebar_path)
+  end
+
+  def sidebar_c_show(path)
+    if current_page?(path)
+      'c-show'
+    end
+  end
 end
